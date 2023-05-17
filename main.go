f869eb9ba32379f9a10550e058af01b011fd29ef
Shaptarshi-Basu <shapbasu@gmail.com>
2023-05-17 07:52:47 +0300

    Main function to prompt for the hands

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"poker/operations"
 )
 
 func main() {
-	args := os.Args[1:] // Get all command-line arguments except for the program name
+	var fh, sh string
 
-	if len(args) < 2 {
-		fmt.Println("Please provide two string arguments as the two hands of the poker.")
-		return
-	}
-	fh := args[0]
-	sh := args[1]
+	fmt.Println("Enter the first input:")
+	fmt.Scan(&fh)
+
+	fmt.Println("Enter the second input:")
+	fmt.Scan(&sh)
+
 	player1 := operations.Player{Hand_Number: "Hand 1", Hand: fh}
 	player2 := operations.Player{Hand_Number: "Hand 2", Hand: sh}
 	result, err := operations.GetWinner(player1, player2)
